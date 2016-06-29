f676db9dd543fde1aace1999e63bfbcffb97e4c1
César <cesarouchida@gmail.com>
2016-06-29 02:07:11 -0300

    Deploy

--- src/main/java/com/example/configuration/AppWebConfiguration.java
+++ src/main/java/com/example/configuration/AppWebConfiguration.java
@@ -1,8 +1,10 @@
 package com.example.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@Profile("prod")
 public class AppWebConfiguration extends WebMvcConfigurerAdapter{
 }

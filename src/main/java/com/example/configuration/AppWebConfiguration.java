1c9ed1a77a1084b46e3064ed38c96dd15ce3454e
César <cesarouchida@gmail.com>
2016-06-29 02:45:26 -0300

    Deploy

--- src/main/java/com/example/configuration/AppWebConfiguration.java
+++ src/main/java/com/example/configuration/AppWebConfiguration.java
@@ -1,31 +1,23 @@
 package com.example.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import javax.sql.DataSource;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 @Configuration
 @Profile("prod")
 public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 
-    @Autowired
-    private Environment environment;
-
-    @Bean
-    public DataSource dataSource() throws URISyntaxException {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        URI dbUrl = new URI(environment.getProperty("DATABASE_URL"));
-        dataSource.setUsername(dbUrl.getUserInfo().split(":")[0]);
-        dataSource.setPassword(dbUrl.getUserInfo().split(":")[1]);
-        return dataSource;
-    }
+//    @Autowired
+//    private Environment environment;
+//
+//    @Bean
+//    public DataSource dataSource() throws URISyntaxException {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("org.postgresql.Driver");
+//        URI dbUrl = new URI(environment.getProperty("DATABASE_URL"));
+//        dataSource.setUsername(dbUrl.getUserInfo().split(":")[0]);
+//        dataSource.setPassword(dbUrl.getUserInfo().split(":")[1]);
+//        return dataSource;
+//    }
 }

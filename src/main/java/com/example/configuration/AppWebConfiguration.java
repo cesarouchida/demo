06f927014336c01bff69184465c2f1e224981c5e
César <cesarouchida@gmail.com>
2016-06-30 00:02:53 -0300

    Deploy

--- src/main/java/com/example/configuration/AppWebConfiguration.java
+++ src/main/java/com/example/configuration/AppWebConfiguration.java
@@ -13,19 +13,5 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 @Configuration
-@Profile("prod")
 public class AppWebConfiguration extends WebMvcConfigurerAdapter {
-
-    @Autowired
-    private Environment environment;
-
-    @Bean
-    public DataSource dataSource() throws URISyntaxException {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        URI dbUrl = new URI(environment.getProperty("DATABASE_URL"));
-        dataSource.setUsername(dbUrl.getUserInfo().split(":")[0]);
-        dataSource.setPassword(dbUrl.getUserInfo().split(":")[1]);
-        return dataSource;
-    }
 }

e3cb4df4678bcb8d74729f4d5c9fc77a32fb19f2
César <cesarouchida@gmail.com>
2016-06-30 02:11:52 -0300

    Deploy

--- src/main/java/com/example/configuration/JPAProductionConfiguration.java
+++ src/main/java/com/example/configuration/JPAProductionConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.configuration;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,6 +12,22 @@ import java.net.URISyntaxException;
 @Configuration
 @Profile("prod")
 public class JPAProductionConfiguration {
+
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
 
 //    @Autowired
 //    private Environment environment;

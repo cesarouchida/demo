22aa6bcad41e2f302f97d46760b1d9a74b9859c9
César <cesarouchida@gmail.com>
2016-06-30 02:52:37 -0300

    Deploy

--- src/main/java/com/example/configuration/JPAProductionConfiguration.java
+++ src/main/java/com/example/configuration/JPAProductionConfiguration.java
@@ -16,45 +16,45 @@ import java.net.URISyntaxException;
 @Profile("prod")
 public class JPAProductionConfiguration {
 
-    @Bean
-    public BasicDataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-
-        System.out.println(username);
-        System.out.println(password);
-        System.out.println(dbUrl);
-
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
-
-        return basicDataSource;
-    }
-
-//    @Autowired
-//    private Environment environment;
-//
 //    @Bean
-//    public DataSource dataSource() throws URISyntaxException {
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        dataSource.setDriverClassName("org.postgresql.Driver");
+//    public BasicDataSource dataSource() throws URISyntaxException {
+//        URI dbUri = new URI(System.getenv("DATABASE_URL"));
 //
-//        URI dbUrl = new URI(environment.getProperty("dcg33dmvcn7p1a"));
-//        dataSource.setUrl("jdbc:postgresql://" +
-//                            dbUrl.getHost() +  ":" +
-//                            dbUrl.getPort() +
-//                            dbUrl.getPath()
-//                        );
+//        String username = dbUri.getUserInfo().split(":")[0];
+//        String password = dbUri.getUserInfo().split(":")[1];
+//        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 //
-//        dataSource.setUsername(dbUrl.getUserInfo().split(":")[0]);
-//        dataSource.setPassword(dbUrl.getUserInfo().split(":")[1]);
-//        return dataSource;
+//        System.out.println(username);
+//        System.out.println(password);
+//        System.out.println(dbUrl);
+//
+//        BasicDataSource basicDataSource = new BasicDataSource();
+//        basicDataSource.setUrl(dbUrl);
+//        basicDataSource.setUsername(username);
+//        basicDataSource.setPassword(password);
+//
+//        return basicDataSource;
 //    }
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public DataSource dataSource() throws URISyntaxException {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+
+        URI dbUrl = new URI(environment.getProperty("dcg33dmvcn7p1a"));
+        dataSource.setUrl("jdbc:postgresql://" +
+                            dbUrl.getHost() +  ":" +
+                            dbUrl.getPort() +
+                            dbUrl.getPath()
+                        );
+
+        dataSource.setUsername(dbUrl.getUserInfo().split(":")[0]);
+        dataSource.setPassword(dbUrl.getUserInfo().split(":")[1]);
+        return dataSource;
+    }
 //
 //    @Bean
 //    @Profile("prod")

df7f5b7952ce152db9f016539a9d8f40f0bf885d
César <cesarouchida@gmail.com>
2016-06-30 02:46:07 -0300

    Deploy

--- src/main/java/com/example/configuration/AppWebConfiguration.java
+++ src/main/java/com/example/configuration/AppWebConfiguration.java
@@ -16,23 +16,23 @@ import java.net.URISyntaxException;
 @Configuration
 public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 
-    @Bean
-    public BasicDataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-
-        System.out.println(username);
-        System.out.println(password);
-        System.out.println(dbUrl);
-
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
-
-        return basicDataSource;
-    }
+//    @Bean
+//    public BasicDataSource dataSource() throws URISyntaxException {
+//        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+//
+//        String username = dbUri.getUserInfo().split(":")[0];
+//        String password = dbUri.getUserInfo().split(":")[1];
+//        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+//
+//        System.out.println(username);
+//        System.out.println(password);
+//        System.out.println(dbUrl);
+//
+//        BasicDataSource basicDataSource = new BasicDataSource();
+//        basicDataSource.setUrl(dbUrl);
+//        basicDataSource.setUsername(username);
+//        basicDataSource.setPassword(password);
+//
+//        return basicDataSource;
+//    }
 }
